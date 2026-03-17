0bd308225af8911c83dbdcffd26fe6ca41be5591
levimenezesthz <levimenezes.thz@gmail.com>
2026-03-17 11:52:16 -0300

    Update portainer.sh

--- portainer.sh
+++ portainer.sh
@@ -4,7 +4,8 @@ export DEBIAN_FRONTEND=noninteractive
 
 ################# ATUALIZAÇÃO DOS REPOSITÓRIOS
 echo "\e[34m[- ATUALIZANDO REPOSITÓRIOS\e[0m"
-apt -yq update; then
+apt -yq update
+if [ $? -eq 0 ]; then
     echo "\e[32m[ SUCESSO ]\e[0m - Repositorios atualizados."
 else
     echo "\e[31m[ ERRO ]\e[0m - Um ou mais repositorio não atualizado."
@@ -12,7 +13,8 @@ fi
 
 ################# ATUALIZAÇÃO DO SISTEMA
 echo "\e[34m[- ATUALIZANDO SISTEMA OPERACIONAL\e[0m"
-apt -yq upgrade; then
+apt -yq upgrade
+if [ $? -eq 0 ]; then
     echo "\e[32m[ SUCESSO ]\e[0m - Sistema atualizado."
 else
     echo "\e[31m[ ERRO ]\e[0m - Sistema não atualizado."
@@ -20,7 +22,8 @@ fi
 
 ################# INSTALAÇÃO DO DOCKER
 echo "\e[34m[- INSTALANDO DOCKER SWARM\e[0m"
-apt -yq install docker.io; then
+apt -yq install docker.io
+if [ $? -eq 0 ]; then
     echo "\e[32m[ SUCESSO ]\e[0m - Docker Swarm instalado"
 else
     echo "\e[31m[ ERRO ]\e[0m - Docker Swarm não instalado."
@@ -29,7 +32,8 @@ fi
 
 ################# CONFIGURANDO DOCKER SWARM
 echo "\e[34m[- CONFIGURANDO DOCKER SWARM\e[0m"
-docker swarm init; then
+docker swarm init
+if [ $? -eq 0 ]; then
     echo "\e[32m[ SUCESSO ]\e[0m - Docker Swarm configurado"
 else
     echo "\e[31m[ ERRO ]\e[0m - Docker Swarm não configurado."
@@ -38,27 +42,32 @@ fi
 ################# INSTALANDO PORTAINER CE
 echo "\e[34m[- INSTALANDO PORTAINER CE\e[0m"
 
-curl -L https://raw.githubusercontent.com/levimenezesthz/scripts/refs/heads/main/portainer.yaml -o portainer.yaml; then
+curl -L https://raw.githubusercontent.com/levimenezesthz/scripts/refs/heads/main/portainer.yaml -o portainer.yaml
+if [ $? -eq 0 ]; then
     echo "\e[32m[ SUCESSO ]\e[0m - Stack do portainer carregado"
 else
     echo "\e[31m[ ERRO ]\e[0m - Stack do portainer não carregado."
 fi
 
-docker stack deploy --prune --resolve-image always -c portainer.yaml portainer; then
+docker stack deploy --prune --resolve-image always -c portainer.yaml portainer
+if [ $? -eq 0 ]; then
     echo "\e[32m[ SUCESSO ]\e[0m - Portainer instalado"
 else
     echo "\e[31m[ ERRO ]\e[0m - Portainer não instalado."
 fi
 
+################# INSTALANDO TRAEFIK CE
 echo "\e[34m[- INSTALANDO TRAEFIK CE\e[0m"
 
-curl -L https://raw.githubusercontent.com/levimenezesthz/scripts/refs/heads/main/traefik.yaml -o traefik.yaml; then
+curl -L https://raw.githubusercontent.com/levimenezesthz/scripts/refs/heads/main/traefik.yaml -o traefik.yaml
+if [ $? -eq 0 ]; then
     echo "\e[32m[ SUCESSO ]\e[0m - Stack do traefik carregado"
 else
     echo "\e[31m[ ERRO ]\e[0m - Stack do traefik não carregado."
 fi
 
-docker stack deploy --prune --resolve-image always -c traefik.yaml traefik; then
+docker stack deploy --prune --resolve-image always -c traefik.yaml traefik
+if [ $? -eq 0 ]; then
     echo "\e[32m[ SUCESSO ]\e[0m - Traefik instalado"
 else
     echo "\e[31m[ ERRO ]\e[0m - Traefik não instalado."
