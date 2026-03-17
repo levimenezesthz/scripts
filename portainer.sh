e4c4540683ad56a8381723e58be57849d788bebf
levimenezesthz <levimenezes.thz@gmail.com>
2026-03-17 17:37:05 -0300

    Enable interpretation of escape sequences in echo
    Updated echo statements to use -e flag for enabling interpretation of backslash escapes.

--- portainer.sh
+++ portainer.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-RED='\033[0;31m'
 GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
+RED='\033[0;31m'
+NC='\033[0m'
 BLUE='\033[0;34m'
-NC='\033[0m' # No Color (reset)
 
 export DEBIAN_FRONTEND=noninteractive
 
@@ -12,27 +11,27 @@ export DEBIAN_FRONTEND=noninteractive
 echo "${BLUE}- ATUALIZANDO REPOSITÓRIOS${NC}"
 apt -yq update > /dev/null 2>&1
 if [ $? -eq 0 ]; then
-    echo "${GREEN}[ SUCESSO ]${NC} - Repositorios atualizados."
+    echo -e "${GREEN}[ SUCESSO ]${NC} - Repositorios atualizados."
 else
-    echo "${RED}[ ERRO ]${NC} - Um ou mais repositorio não atualizado."
+    echo -e "${RED}[ ERRO ]${NC} - Um ou mais repositorio não atualizado."
 fi
 
 ################# ATUALIZAÇÃO DO SISTEMA
 echo "${BLUE}[- ATUALIZANDO SISTEMA OPERACIONAL${NC}"
 apt -yq upgrade > /dev/null 2>&1
 if [ $? -eq 0 ]; then
-    echo "${GREEN}[ SUCESSO ]${NC} - Sistema atualizado."
+    echo -e "${GREEN}[ SUCESSO ]${NC} - Sistema atualizado."
 else
-    echo "${RED}[ ERRO ]${NC} - Sistema não atualizado."
+    echo -e "${RED}[ ERRO ]${NC} - Sistema não atualizado."
 fi
 
 ################# INSTALAÇÃO DO DOCKER
 echo "${BLUE}[- INSTALANDO DOCKER SWARM${NC}"
 apt -yq install docker.io > /dev/null 2>&1
 if [ $? -eq 0 ]; then
-    echo "${GREEN}[ SUCESSO ]${NC} - Docker Swarm instalado"
+    echo -e "${GREEN}[ SUCESSO ]${NC} - Docker Swarm instalado"
 else
-    echo "${RED}[ ERRO ]${NC} - Docker Swarm não instalado."
+    echo -e "${RED}[ ERRO ]${NC} - Docker Swarm não instalado."
 fi
 
 
@@ -40,9 +39,9 @@ fi
 echo "${BLUE}[- CONFIGURANDO DOCKER SWARM${NC}"
 docker swarm init > /dev/null 2>&1
 if [ $? -eq 0 ]; then
-    echo "${GREEN}[ SUCESSO ]${NC} - Docker Swarm configurado"
+    echo -e "${GREEN}[ SUCESSO ]${NC} - Docker Swarm configurado"
 else
-    echo "${RED}[ ERRO ]${NC} - Docker Swarm não configurado."
+    echo -e "${RED}[ ERRO ]${NC} - Docker Swarm não configurado."
 fi
 
 ################# INSTALANDO PORTAINER CE
@@ -50,16 +49,16 @@ echo "${BLUE}[- INSTALANDO PORTAINER CE${NC}"
 
 curl -L https://raw.githubusercontent.com/levimenezesthz/scripts/refs/heads/main/portainer.yaml -o portainer.yaml > /dev/null 2>&1
 if [ $? -eq 0 ]; then
-    echo "${GREEN}[ SUCESSO ]${NC} - Stack do portainer carregado"
+    echo -e "${GREEN}[ SUCESSO ]${NC} - Stack do portainer carregado"
 else
-    echo "${RED}[ ERRO ]${NC} - Stack do portainer não carregado."
+    echo -e "${RED}[ ERRO ]${NC} - Stack do portainer não carregado."
 fi
 
 docker stack deploy --prune --resolve-image always -c portainer.yaml portainer > /dev/null 2>&1
 if [ $? -eq 0 ]; then
-    echo "${GREEN}[ SUCESSO ]${NC} - Portainer instalado"
+    echo -e "${GREEN}[ SUCESSO ]${NC} - Portainer instalado"
 else
-    echo "${RED}[ ERRO ]${NC} - Portainer não instalado."
+    echo -e "${RED}[ ERRO ]${NC} - Portainer não instalado."
 fi
 
 ################# INSTALANDO TRAEFIK CE
@@ -67,15 +66,15 @@ echo "${BLUE}[- INSTALANDO TRAEFIK CE${NC}"
 
 curl -L https://raw.githubusercontent.com/levimenezesthz/scripts/refs/heads/main/traefik.yaml -o traefik.yaml > /dev/null 2>&1
 if [ $? -eq 0 ]; then
-    echo "${GREEN}[ SUCESSO ]${NC} - Stack do traefik carregado"
+    echo -e "${GREEN}[ SUCESSO ]${NC} - Stack do traefik carregada"
 else
-    echo "${RED}[ ERRO ]${NC} - Stack do traefik não carregado."
+    echo -e "${RED}[ ERRO ]${NC} - Stack do traefik não carregado."
 fi
 
 docker stack deploy --prune --resolve-image always -c traefik.yaml traefik > /dev/null 2>&1
 if [ $? -eq 0 ]; then
-    echo "${GREEN}[ SUCESSO ]${NC} - Traefik instalado"
+    echo -e "${GREEN}[ SUCESSO ]${NC} - Traefik instalada"
 else
-    echo "${RED}[ ERRO ]${NC} - Traefik não instalado."
+    echo -e "${RED}[ ERRO ]${NC} - Traefik não instalado."
 fi
 
