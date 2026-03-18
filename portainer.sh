5aec427b933bad382df6e36be5c5b1140522ced3
levimenezesthz <levimenezes.thz@gmail.com>
2026-03-17 21:16:17 -0300

    Update portainer.sh

--- portainer.sh
+++ portainer.sh
@@ -7,74 +7,97 @@ BLUE='\033[0;34m'
 
 export DEBIAN_FRONTEND=noninteractive
 
-################# ATUALIZAÇÃO DOS REPOSITÓRIOS
-echo "${BLUE}[ PROCESS ]${NC} - ATUALIZANDO REPOSITÓRIOS"
+####################################################################
+# PREINSTALAÇÃO #################################################### 
+####################################################################
+echo -e "${BLUE}[ PROCESS ]${NC} - ATUALIZANDO SISTEMA"
 apt -yq update > /dev/null 2>&1
 if [ $? -eq 0 ]; then
     echo -e "${GREEN}[ SUCESSO ]${NC} - Repositorios atualizados."
 else
-    echo -e "${RED}[ ERRO ]${NC} - Um ou mais repositorio não atualizado."
+    echo -e "${RED}[ _ERRO_ ]${NC} - Um ou mais repositorio NÃO atualizado."
 fi
 
-################# ATUALIZAÇÃO DO SISTEMA
-echo "${BLUE}[ PROCESS ]${NC} - ATUALIZANDO SISTEMA OPERACIONAL"
 apt -yq upgrade > /dev/null 2>&1
 if [ $? -eq 0 ]; then
     echo -e "${GREEN}[ SUCESSO ]${NC} - Sistema atualizado."
 else
-    echo -e "${RED}[ ERRO ]${NC} - Sistema não atualizado."
+    echo -e "${RED}[ _ERRO_ ]${NC} - Sistema NÃO atualizado."
 fi
 
-################# INSTALAÇÃO DO DOCKER
-echo "${BLUE}[ PROCESS ]${NC} - INSTALANDO DOCKER SWARM"
+####################################################################
+# INSTALAÇÃO DO DOCKER SWARM ####################################### 
+####################################################################
+
+echo -e "${BLUE}[ PROCESS ]${NC} - INSTALANDO DOCKER SWARM"
+
+sudo mkdir -p /etc/apt/keyrings && curl -fsSL https://download.docker.com | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+    echo -e "${GREEN}[ SUCESSO ]${NC} - Chave do repositorio Docker configurada."
+else
+    echo -e "${RED}[ _ERRO_ ]${NC} - Chave do repositorio Docker NÃO configurada."
+fi
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list && apt update > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+    echo -e "${GREEN}[ SUCESSO ]${NC} - Repositorio oficial do Docker configurado."
+else
+    echo -e "${RED}[ _ERRO_ ]${NC} - Repositorio oficial do Docker NÃO configurado."
+fi
+
 apt -yq install docker.io > /dev/null 2>&1
 if [ $? -eq 0 ]; then
-    echo -e "${GREEN}[ SUCESSO ]${NC} - Docker Swarm instalado"
+    echo -e "${GREEN}[ SUCESSO ]${NC} - Docker Swarm instalado."
 else
-    echo -e "${RED}[ ERRO ]${NC} - Docker Swarm não instalado."
+    echo -e "${RED}[ _ERRO_ ]${NC} - Docker Swarm NÃO instalado."
 fi
 
-
-################# CONFIGURANDO DOCKER SWARM
-echo "${BLUE}[ PROCESS ]${NC} - CONFIGURANDO DOCKER SWARM"
 docker swarm init > /dev/null 2>&1
 if [ $? -eq 0 ]; then
-    echo -e "${GREEN}[ SUCESSO ]${NC} - Docker Swarm configurado"
+    echo -e "${GREEN}[ SUCESSO ]${NC} - Docker Swarm configurado."
 else
-    echo -e "${RED}[ ERRO ]${NC} - Docker Swarm não configurado."
+    echo -e "${RED}[ _ERRO_ ]${NC} - Docker Swarm NÃO configurado."
 fi
 
-################# INSTALANDO PORTAINER CE
-echo "${BLUE}[ PROCESS ]${NC} - INSTALANDO PORTAINER CE"
+####################################################################
+# INSTALAÇÃO DO PORTAINER ########################################## 
+####################################################################
+
+echo -e "${BLUE}[ PROCESS ]${NC} - INSTALANDO PORTAINER CE"
 
 curl -L https://raw.githubusercontent.com/levimenezesthz/scripts/refs/heads/main/portainer.yaml -o portainer.yaml > /dev/null 2>&1
 if [ $? -eq 0 ]; then
     echo -e "${GREEN}[ SUCESSO ]${NC} - Stack do portainer carregado"
 else
-    echo -e "${RED}[ ERRO ]${NC} - Stack do portainer não carregado."
+    echo -e "${RED}[ _ERRO_ ]${NC} - Stack do portainer NÃO carregado."
 fi
 
 docker stack deploy --prune --resolve-image always -c portainer.yaml portainer > /dev/null 2>&1
 if [ $? -eq 0 ]; then
-    echo -e "${GREEN}[ SUCESSO ]${NC} - Portainer instalado"
+    echo -e "${GREEN}[ SUCESSO ]${NC} - Portainer instalado."
 else
-    echo -e "${RED}[ ERRO ]${NC} - Portainer não instalado."
+    echo -e "${RED}[ _ERRO_ ]${NC} - Portainer NÃO instalado."
 fi
 
-################# INSTALANDO TRAEFIK CE
-echo "${BLUE}[ PROCESS ]${NC} - INSTALANDO TRAEFIK CE"
+####################################################################
+# INSTALAÇÃO DO TRAEFIK ############################################ 
+####################################################################
+
+echo -e "${BLUE}[ PROCESS ]${NC} - INSTALANDO TRAEFIK"
 
 curl -L https://raw.githubusercontent.com/levimenezesthz/scripts/refs/heads/main/traefik.yaml -o traefik.yaml > /dev/null 2>&1
 if [ $? -eq 0 ]; then
-    echo -e "${GREEN}[ SUCESSO ]${NC} - Stack do traefik carregada"
+    echo -e "${GREEN}[ SUCESSO ]${NC} - Stack do traefik carregada."
 else
-    echo -e "${RED}[ ERRO ]${NC} - Stack do traefik não carregado."
+    echo -e "${RED}[ _ERRO_ ]${NC} - Stack do traefik NÃO carregado."
 fi
 
 docker stack deploy --prune --resolve-image always -c traefik.yaml traefik > /dev/null 2>&1
 if [ $? -eq 0 ]; then
-    echo -e "${GREEN}[ SUCESSO ]${NC} - Traefik instalada"
+    echo -e "${GREEN}[ SUCESSO ]${NC} - Traefik instalada."
 else
-    echo -e "${RED}[ ERRO ]${NC} - Traefik não instalado."
+    echo -e "${RED}[ _ERRO_ ]${NC} - Traefik NÃO instalado."
 fi
 
